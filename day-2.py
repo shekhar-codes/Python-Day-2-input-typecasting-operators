85782fde78dfac9d0c3bcec42c7054c541447bf7
Shekhar-codes <shekharxsingh57@gmail.com>
2025-07-18 18:13:53 +0530

    Added input handling, type casting examples, and operator usage in Python
    - Implemented examples for taking user input using the input() function
    - Demonstrated explicit and implicit type casting with int(), float(), str()
    - Added code snippets for arithmetic, comparison, logical, and assignment operators
    - Included comments for better understanding and beginner-friendly learning

---
+++ day-2.py
@@ -0,0 +1,92 @@
+"""Typecasting = it is a method by which we can convert one datatype into one another datatype
+ variable = type of datatype that you want to convert one into another(value that is going to convert)"""
+ 
+a = 45
+print(type(a))
+
+print(type(str(a)))
+
+# input function = 
+
+
+a = input("enter your age",)
+print(a)
+b = int(input("enter your age",))    # here we use typecasting method to change datatype to another.
+print(b)
+
+"""operators = 
+types of operators
+1) airthmetic operator (+ , - , * , / , ** , %)
++ = addition
+- =  subtraction
+* = multiplication
+/ = dividend
+** = power operator
+% = remainder
+2) assignment operator (++ , -= , *= , /= , **= , %=)
+3) comparison operator(< , > ,<= ,>=)
+"""
+
+# additon operators
+a = 48
+b = 10
+c = a+b
+print(c)
+
+# subtarction operator
+a = 48
+b = 10
+c = a-b
+print(c)
+
+# multiplication operators
+a = 48
+b = 10
+c = a*b
+print(c)
+
+# dividend operator
+a = 48
+b = 10
+c = a/b
+print(c)
+
+# power operator
+a = 8
+b = 2
+c = a**b  # here a**b mean 8 to the power 2
+print(c)
+
+# remainder operator
+a = 48
+b = 10
+c = a%b
+print(c)
+
+
+
+# assignment operator
+
+a = 48
+a += 10
+print(a)
+
+a = 48
+a -= 10
+print(a)
+
+a = 48
+a *= 10
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
